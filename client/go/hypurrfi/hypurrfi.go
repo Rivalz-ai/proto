02b63ab8c84a6561705f1164617db6d025bb4677
suhoangson <son@treehouse.finance>
2025-08-29 09:58:19 +0700

    genearete  hypurrfi proto

---
+++ client/go/hypurrfi/hypurrfi.go
@@ -0,0 +1,17 @@
+package price
+
+import (
+	grpcClient "github.com/Rivalz-ai/proto/client/go/common"
+	"github.com/Rivalz-ai/proto/pb/go/hypurrfi"
+)
+
+var SVC hypurrfi.HypurrfiServiceClient
+
+func NewHypurrfiServiceClient(grpc_server_address string) error {
+	conn, err := grpcClient.NewGRPCClientConn(grpc_server_address)
+	if err != nil {
+		return err
+	}
+	SVC = hypurrfi.NewHypurrfiServiceClient(conn)
+	return nil
+}
